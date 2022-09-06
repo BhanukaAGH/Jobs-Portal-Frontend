0f2abf594d30fdd5f2d964f8b75d558577fa9516
Rivindu2 <rivigith@gmail.com>
2022-09-06 12:15:53 +0530

    Events deisgn

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -1,7 +1,39 @@
 import React from 'react'
+import Navbar from '../components/Home/Navbar'
+import EventsCards from '../components/Candidate/EventsCards'
+import { useSelector } from 'react-redux'
 
 const Events = () => {
-  return <div>Events Page</div>
+  const { authModal } = useSelector((state) => state.ui)
+  return (
+    <div className={`${authModal && 'h-screen overflow-hidden'}`}>
+      <Navbar />
+      <header className='header-container'>
+        <h1 className='header-title'>Event Postings</h1>
+
+        <div className='header-search-container'>
+          <div className='flex items-center w-full space-x-2'>
+            <input
+              type='text'
+              className='header-input'
+              placeholder='Job title or keyword'
+            />
+            <input
+              type='text'
+              className='header-input'
+              placeholder='Location'
+            />
+            <button className='header-search-button'>Search</button>
+          </div>
+        </div>
+      </header>
+      <div class="box-border h-auto w-full pt-16 pr-36 pl-36">
+        {/* card starts here */}
+        <EventsCards />
+        {/* card ends here */}
+      </div>
+    </div>
+  )
 }
 
 export default Events
