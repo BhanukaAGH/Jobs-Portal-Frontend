d0bc0bb7e88c69ab91223b813379a13bf0a56bd6
BhanukaAGH <it20009472@my.sliit.lk>
2022-09-06 15:37:33 +0530

    header styles refactor

--- src/pages/Companies.jsx
+++ src/pages/Companies.jsx
@@ -1,7 +1,30 @@
 import React from 'react'
+import Navbar from '../components/Home/Navbar'
+import { useSelector } from 'react-redux'
 
 const Companies = () => {
-  return <div>Companies Page</div>
+  const { authModal } = useSelector((state) => state.ui)
+
+  return (
+    <div className={`${authModal && 'h-screen overflow-hidden'}`}>
+      <Navbar />
+      <header className='header-container'>
+        <h1 className='header-title'>Find great places to work</h1>
+
+        <div className='header-search-container'>
+          <div className='flex items-center w-full space-x-2'>
+            <input
+              type='text'
+              className='header-input'
+              placeholder='Company name or keyword'
+            />
+
+            <button className='header-search-button'>Search</button>
+          </div>
+        </div>
+      </header>
+    </div>
+  )
 }
 
 export default Companies
--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -1,7 +1,30 @@
 import React from 'react'
+import Navbar from '../components/Home/Navbar'
+import { useSelector } from 'react-redux'
 
 const Events = () => {
-  return <div>Events Page</div>
+  const { authModal } = useSelector((state) => state.ui)
+
+  return (
+    <div className={`${authModal && 'h-screen overflow-hidden'}`}>
+      <Navbar />
+      <header className='header-container'>
+        <h1 className='header-title'>Find all events here</h1>
+
+        <div className='header-search-container'>
+          <div className='flex items-center w-full space-x-2'>
+            <input
+              type='text'
+              className='header-input'
+              placeholder='Event name or keyword'
+            />
+
+            <button className='header-search-button'>Search</button>
+          </div>
+        </div>
+      </header>
+    </div>
+  )
 }
 
 export default Events
