b68f9b69dc54679ac780915a7bb3043cd98ce133
BhanukaAGH <it20009472@my.sliit.lk>
2022-09-07 10:09:32 +0530

    code format

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -16,7 +16,10 @@ const Events = () => {
           className='absolute top-0 left-0 object-cover w-full h-full opacity-40 select-none'
         />
 
-        <h1 className='text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold mb-4 md:mb-12 text-center text-white '>  Event Postings</h1>
+        <h1 className='text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold mb-4 md:mb-12 text-center text-white '>
+          {' '}
+          Event Postings
+        </h1>
 
         <div className='header-search-container'>
           <div className='flex items-center w-full space-x-2'>
@@ -25,16 +28,12 @@ const Events = () => {
               className='header-input'
               placeholder='Event title or keyword'
             />
-            <input
-              type='text'
-              className='header-input'
-              placeholder='Company'
-            />
+            <input type='text' className='header-input' placeholder='Company' />
             <button className='header-search-button'>Search</button>
           </div>
         </div>
       </header>
-      <div class="box-border h-auto w-full pt-16 pr-36 pl-36">
+      <div class='box-border h-auto w-full pt-16 pr-36 pl-36'>
         {/* card starts here */}
         <EventsCards />
         {/* card ends here */}
