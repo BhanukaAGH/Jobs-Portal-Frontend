c1406bb690041c7d21d241003903143497a90021
BhanukaAGH <it20009472@my.sliit.lk>
2022-09-08 15:51:38 +0530

    job redux create

--- src/app/store.js
+++ src/app/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit'
 import uiReducer from '../features/ui/uiSlice'
 import authReducer from '../features/auth/authSlice'
 import companyReducer from '../features/company/companySlice'
+import jobReducer from '../features/job/jobSlice'
 
 export const store = configureStore({
   reducer: {
     ui: uiReducer,
     auth: authReducer,
     company: companyReducer,
+    job: jobReducer,
   },
 })
