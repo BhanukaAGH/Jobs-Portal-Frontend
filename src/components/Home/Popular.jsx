ba034b0d75a9ff90f6b9affcd3c7cc8925bba5b9
BhanukaAGH <it20009472@my.sliit.lk>
2022-09-17 16:05:49 +0530

    popular jobs styles update

--- src/components/Home/Popular.jsx
+++ src/components/Home/Popular.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
-import GoogleImage from '../../assets/GImage.webp'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import api from '../../utils/api'
 
 const Popular = () => {
+  const [popularJobs, setPopularJobs] = useState([])
+
+  useEffect(() => {
+    const getPopularJobs = async () => {
+      const res = await api.get('/applicant/getPopularJobs')
+      setPopularJobs(res.data)
+    }
+
+    getPopularJobs()
+  }, [])
+
   return (
     <div className='flex flex-col justify-center items-center py-16'>
       <div className='flex flex-col md:flex-row items-center justify-between w-5/6 lg:w-4/6 mb-8'>
@@ -13,27 +25,30 @@ const Popular = () => {
             Check our featured jobs from popular companies. Start applying now.
           </p>
         </div>
-        <button className='text-white font-[Poppins] bg-[#312ECB] px-4 py-2 rounded-md'>
+        <Link
+          to={'/jobs'}
+          className='text-white font-[Poppins] bg-[#312ECB] px-4 py-2 rounded-md'
+        >
           View all Jobs
-        </button>
+        </Link>
       </div>
 
       <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 w-5/6 lg:w-4/6 gap-x-6 xl:gap-x-8 2xl:gap-x-20 gap-y-4 md:gap-y-12'>
-        {[...Array(6)].map((item, index) => (
+        {popularJobs?.map((data) => (
           <div
-            key={index}
-            className='flex flex-col items-center justify-center rounded-md w-full h-80 cursor-pointer bg-[#4285F4]/20 px-2 sm:px-4 hover:bg-[#4285F4]/80'
+            key={data.job._id}
+            className='flex flex-col items-center justify-center rounded-lg w-full h-80 cursor-pointer bg-[#4285F4]/10 px-2 sm:px-4 shadow-lg hover:shadow-xl hover:shadow-blue-200'
           >
             <img
-              src={GoogleImage}
+              src={data.job.company.photoUrl}
               alt='google-logo'
               className='w-14 lg:w-20 mb-3'
             />
             <h1 className='text-2xl font-[Poppins] font-semibold text-center text-black'>
-              Software Engineer
+              {data.job.jobTitle}
             </h1>
             <p className='font-[Poppins] text-[#6B7E8B] text-center mb-6'>
-              We are looking for a senior software engineer
+              We are looking for a {data.job.jobTitle}
             </p>
             <button className='text-white bg-[#0A2537] rounded-md px-3 py-1 text-center'>
               Apply Now
