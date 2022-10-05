c8a8abbc54a5c12bcf0876919396399a0090be71
HasithKovinda <it20032906@my.sliit.lk>
2022-10-05 23:34:21 +0530

    adminManagement done

--- src/components/Admin/AdminDashboard.jsx
+++ src/components/Admin/AdminDashboard.jsx
@@ -1,23 +1,36 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import Create from "./Account/Create";
+import ViewAccounts from "./Account/ViewAccounts";
 
 const AdminDashboard = () => {
-  return (
-    <div className='w-full h-full bg-[#F9FAFF]'>
-      {/* Title Section */}
-      <div className='dashboard-title'>
-        <h3 className='text-lg md:text-2xl xl:text-3xl'>Dashboard</h3>
-      </div>
+  const [model, setModel] = useState(false);
 
+  const modelOpen = () => {
+    setModel(!model);
+  };
+  return (
+    <div className="w-full h-full bg-[#F9FAFF]">
+      {/* Title Section */}
+      <div className="dashboard-title">
+        <h3 className="text-lg md:text-2xl xl:text-3xl">Dashboard</h3>
+        <button className="admin-btn bg-black mt-3 ml-3" onClick={modelOpen}>
+          Create Admin
+        </button>
+      </div>
       {/* Dashboard Content */}
-      <div className='dashboard-content'>
-        <div className='flex flex-1 overflow-hidden relative h-full w-full'>
-          <div className='absolute inset-0 overflow-auto !scrollbar-thin !scrollbar-track-gray-200 !scrollbar-thumb-gray-800'>
+
+      <div className="dashboard-content">
+        <div className="flex flex-1 overflow-hidden relative h-full w-full">
+          <div className="absolute inset-0 overflow-auto !scrollbar-thin !scrollbar-track-gray-200 !scrollbar-thumb-gray-800">
             {/* Type Code here */}
+            <ViewAccounts />
+            {model ? <Create modelOpen={modelOpen} /> : null}
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default AdminDashboard
+export default AdminDashboard;
