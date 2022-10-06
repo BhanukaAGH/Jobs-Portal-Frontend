bb538cf842278407754c045a9a0a6761cef94e57
HasithKovinda <it20032906@my.sliit.lk>
2022-10-06 23:45:59 +0530

    admin company part complete

--- src/components/Admin/AdminCompany.jsx
+++ src/components/Admin/AdminCompany.jsx
@@ -1,23 +1,24 @@
-import React from 'react'
+import React from "react";
+import CompanyView from "./company/CompanyView";
 
 const AdminCompany = () => {
   return (
-    <div className='w-full h-full bg-[#F9FAFF]'>
+    <div className="w-full h-full bg-[#F9FAFF]">
       {/* Title Section */}
-      <div className='dashboard-title'>
-        <h3 className='text-lg md:text-2xl xl:text-3xl'>Companies Details</h3>
+      <div className="dashboard-title">
+        <h3 className="text-lg md:text-2xl xl:text-3xl">Companies Details</h3>
       </div>
 
       {/* Dashboard Content */}
-      <div className='dashboard-content'>
-        <div className='flex flex-1 overflow-hidden relative h-full w-full'>
-          <div className='absolute inset-0 overflow-auto !scrollbar-thin !scrollbar-track-gray-200 !scrollbar-thumb-gray-800'>
-            {/* Type Code here */}
+      <div className="dashboard-content">
+        <div className="flex flex-1 overflow-hidden relative h-full w-full">
+          <div className="absolute inset-0 overflow-auto !scrollbar-thin !scrollbar-track-gray-200 !scrollbar-thumb-gray-800">
+            <CompanyView />
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default AdminCompany
+export default AdminCompany;
---
+++ src/components/Admin/company/UpdateStatus.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { useState } from "react";
+import { AiOutlineClose } from "react-icons/ai";
+
+const UpdateStatus = ({ updateView, updateStatus }) => {
+  const [status, setStatus] = useState("Verified");
+  const closeStatus = () => {
+    updateView();
+  };
+
+  const updateStatusValue = () => {
+    if (status === "Verified") {
+      updateStatus(true);
+      return;
+    }
+    updateStatus(false);
+  };
+  return (
+    <div className="absolute inset-0 flex items-center justify-center bg-black  bg-opacity-50 ">
+      <div className="rounded-lg bg-white py-4 px-6 border-1  border-black rounded-lg w-1/2">
+        <div className="flex items-center justify-between border-b-4 border-green-500 pb-5">
+          <h2>Update Company Status</h2>
+          <AiOutlineClose onClick={closeStatus} className="text-red-600" />
+        </div>
+        <div className="mt-5 flex justify-center items-center">
+          <label htmlFor="" className="mr-4 text-lg">
+            Status
+          </label>
+          <select
+            name="status"
+            id="status"
+            value={status}
+            onChange={(e) => setStatus(e.target.value)}
+          >
+            <option value="Verified">Verified</option>
+            <option value="Not Verified">Not Verified</option>
+          </select>
+        </div>
+        <div className="flex justify-center mt-5">
+          <button
+            className="admin-btn bg-green-600"
+            onClick={updateStatusValue}
+          >
+            Update
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UpdateStatus;
